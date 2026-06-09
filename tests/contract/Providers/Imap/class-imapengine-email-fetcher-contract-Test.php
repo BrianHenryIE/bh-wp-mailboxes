d6852f76414d321097cdb7dd1b096bca06ac6a37
Brian Henry <BrianHenryIE@gmail.com>
2026-06-09 11:03:02 -0700

    Update class-imapengine-email-fetcher-contract-Test.php

--- tests/contract/Providers/Imap/class-imapengine-email-fetcher-contract-Test.php
+++ tests/contract/Providers/Imap/class-imapengine-email-fetcher-contract-Test.php
@@ -13,20 +13,24 @@ use BrianHenryIE\WP_Mailboxes\Account_Credentials_Interface;
 use BrianHenryIE\WP_Mailboxes\Providers\Imap\ImapEngine_Imap_Email_Fetcher;
 use BrianHenryIE\WP_Mailboxes\Providers\Imap\IMAP_Credentials_Interface;
 use BrianHenryIE\WP_Mailboxes\Unit_Testcase;
-use BrianHenryIE\WP_Mailboxes\Mailbox_Settings_Defaults_Trait;
-use BrianHenryIE\WP_Mailboxes\Mailbox_Settings_Interface;
+use BrianHenryIE\WP_Mailboxes\Email_Account_Settings_Defaults_Trait;
+use BrianHenryIE\WP_Mailboxes\Email_Account_Settings_Interface;
 use DateTime;
 use DirectoryTree\ImapEngine\Exceptions\Exception as ImapEngineException;
 use DirectoryTree\ImapEngine\Exceptions\ImapCommandException;
 use DirectoryTree\ImapEngine\Exceptions\ImapConnectionFailedException;
 use Dotenv\Dotenv;
+use Illuminate\Support\Collection;
+use ZBateson\MailMimeParser\IMessage;
+use ZBateson\MailMimeParser\MailMimeParser;
+use ZBateson\MailMimeParser\Message\PartFilter;
 
 /**
  * Username and password are read from `.env.secrets`.
  */
 class ImapEngine_Email_Fetcher_Integration_Test extends Unit_Testcase {
 
-	protected Mailbox_Settings_Interface $settings;
+	protected Email_Account_Settings_Interface $settings;
 
 	public function setUp(): void {
 		parent::setUp();
@@ -39,12 +43,11 @@ class ImapEngine_Email_Fetcher_Integration_Test extends Unit_Testcase {
 		$dotenv = Dotenv::createImmutable( $project_root_dir . '/test-credentials/', '.env.secret', true );
 		$dotenv->load();
 
-		/** @var Mailbox_Settings_Interface $settings */
-		$this->settings = new class() implements Mailbox_Settings_Interface {
+		/** @var Email_Account_Settings_Interface $settings */
+		$this->settings = new class() implements Email_Account_Settings_Interface {
+			use Email_Account_Settings_Defaults_Trait;
 
-			use Mailbox_Settings_Defaults_Trait;
-
-			public function get_account_unique_friendly_name(): string {
+			public function get_account_email_address(): string {
 				return 'support@brianhenryie.com';
 			}
 
@@ -71,6 +74,30 @@ class ImapEngine_Email_Fetcher_Integration_Test extends Unit_Testcase {
 	}
 
 	/**
+	 * Tool to create fixtures.
+	 */
+	protected function save_to_file( IMessage $email, string $filepath, bool $include_attachments = true ): void {
+		$attachment_parts      = $email->getAllAttachmentParts();
+		$all_parts             = $email->getAllParts();
+		$non_attachment_parts  = array_filter(
+			$all_parts,
+			fn( $part ) => ! in_array( $part, $attachment_parts, true )
+		);
+		$original_email_string = $include_attachments
+			? implode( ' ', $all_parts )
+			: implode( ' ', $non_attachment_parts );
+
+		file_put_contents( $filepath, $original_email_string );
+
+		$from_file = file_get_contents( $filepath );
+
+		$parser           = new MailMimeParser();
+		$reparsed         = $parser->parse( $from_file, true );
+		$saved_message_id = $reparsed->getMessageId();
+		$this->assertNotEmpty( $saved_message_id );
+	}
+
+	/**
 	 * Dumps the emails from the past [time] into /tests/_data/emails
 	 */
 	public function test_download_emails_for_tests() {
@@ -83,24 +110,56 @@ class ImapEngine_Email_Fetcher_Integration_Test extends Unit_Testcase {
 			$this->fail( $e->getMessage() );
 		}
 
-		$year_in_seconds = 365 * 24 * 60 * 60;
+		$year_in_seconds = 367 * 24 * 60 * 60;
 		$since_unix_time = time() - $year_in_seconds;
 		$since_time      = DateTime::createFromFormat( 'U', $since_unix_time );
-		$emails          = $sut->retrieve_emails( $since_time );
+		$messages        = $sut->retrieve_emails( $since_time, 100 );
 
-		$this->assertNotEmpty( $emails->count() );
+		$this->assertNotEmpty( $messages->count() );
+
+		/** @var IMessage $message */
+		$message = $messages->first();
+		$id      = $message->getMessageId();
+
+		$attachment_parts      = $message->getAllAttachmentParts();
+		$all_parts             = $message->getAllParts();
+		$non_attachment_parts  = array_filter(
+			$all_parts,
+			fn( $part ) => ! in_array( $part, $attachment_parts, true )
+		);
+		$original_email_string = implode( ' ', $non_attachment_parts );
+
+		$parser   = new MailMimeParser();
+		$reparsed = $parser->parse( $original_email_string, true );
+
+		$reparsed_id = $reparsed->getMessageId();
+
+		$this->assertEquals( $reparsed_id, $id );
+
+		return;
+
+		/** @var IMessage $email */
+		foreach ( $messages as $email ) {
+			if ( empty( $email->getMessageId() ) ) {
+				continue;
+			}
+			$this->save_to_file(
+				$email,
+				codecept_root_dir() . 'tests/_data/emails/' . base64_encode( $email->getHeaderValue( 'Message-ID' ) ) . '.eml',
+			);
+		}
 	}
+
 
 	public function test_bad_server(): void {
 
 		$logger = new ColorLogger();
 
-		/** @var Mailbox_Settings_Interface $settings */
-		$settings = new class() implements Mailbox_Settings_Interface {
+		/** @var Email_Account_Settings_Interface $settings */
+		$settings = new class() implements Email_Account_Settings_Interface {
+			use Email_Account_Settings_Defaults_Trait;
 
-			use Mailbox_Settings_Defaults_Trait;
-
-			public function get_account_unique_friendly_name(): string {
+			public function get_account_email_address(): string {
 				return 'support@brianhenryie.com';
 			}
 
@@ -144,12 +203,11 @@ class ImapEngine_Email_Fetcher_Integration_Test extends Unit_Testcase {
 
 		$logger = new ColorLogger();
 
-		/** @var Mailbox_Settings_Interface $settings */
-		$settings = new class() implements Mailbox_Settings_Interface {
+		/** @var Email_Account_Settings_Interface $settings */
+		$settings = new class() implements Email_Account_Settings_Interface {
+			use Email_Account_Settings_Defaults_Trait;
 
-			use Mailbox_Settings_Defaults_Trait;
-
-			public function get_account_unique_friendly_name(): string {
+			public function get_account_email_address(): string {
 				return 'support@brianhenryie.com';
 			}
 
