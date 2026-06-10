02eb5547126bb3ac14ecbd3282dd1c260f715007
Brian Henry <BrianHenryIE@gmail.com>
2026-06-09 20:33:34 -0700

    Rename var

--- includes/api/repositories/class-email-account-wp-post-repository.php
+++ includes/api/repositories/class-email-account-wp-post-repository.php
@@ -7,7 +7,7 @@ use BrianHenryIE\WP_Mailboxes\Email_Account_Settings_Interface;
 
 class Email_Account_WP_Post_Repository extends WP_Post_Repository_Abstract {
 
-	public function save_new( Email_Account_Settings_Interface $mailbox_settings ): Saved_Post {
+	public function save_new( Email_Account_Settings_Interface $email_account_settings ): Saved_Post {
 	}
 	public function get_by_wp_post_id( int $post_id ): BH_Email_Account {
 	}
